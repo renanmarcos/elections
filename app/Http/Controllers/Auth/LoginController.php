aa7c6e7d4fe9a555621828552b7c46290c05b97c
Renan Marcos Ferreira <renan.ferreira18@fatec.sp.gov.br>
2019-06-17 21:40:11 -0300

    Fixes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -34,7 +34,7 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api')->except('logout');
+        $this->middleware('auth')->except('logout');
     }
 
     public function username()
